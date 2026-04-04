e28b08192cbf24c5fe5f8b432f0ec7c7a9177280
Yassine El Barni <yassine.elbarni-etu@etu.univh2c.ma>
2026-04-04 22:09:40 +0100

    Test: Activate 'test' profile for application context test

--- src/test/java/com/storeshop/StoreShopApplicationTests.java
+++ src/test/java/com/storeshop/StoreShopApplicationTests.java
@@ -3,7 +3,10 @@ package com.storeshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("test")
 class StoreShopApplicationTests {
 
   @Test
